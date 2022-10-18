60014cc67ac6717d37555d3a71d5d45da328aeb1
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-10-17 23:42:24 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -61,8 +61,9 @@ Salvador Salazar            </h2>
           Fullstack Web Developer
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Freelancer providing services for programming and design content
-              needs. 
+             Hello, I'm Salvador! 👋 I am a Junior Web Developer in San Antonio , Texas.
+
+📕 Skills: Javascript, HTML, CSS, Bootstrap, JQuery, Java, MySQL, Spring Boot, React.js , Next.js, C++ , C# , PHP Laravel . Exposure to: AWS ,Python
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <AiFillTwitterCircle />
