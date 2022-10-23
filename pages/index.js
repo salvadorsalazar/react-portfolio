f72f2bdad9113519005bb1aca79cc785c0fa894e
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-10-22 22:01:12 -0500

    link updates

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import calc from "../public/calculator.png";
 import lara from "../public/lara.png";
 import poke from "../public/poke.png";
 import movie from "../public/movie.png";
+import food from "../public/food.jpeg";
 
 
 
@@ -161,17 +162,21 @@ Salvador Salazar            </h2>
 
             <div className="basis-1/3 flex-1">
 
-              <div className="py-5"> <p className=" text-center bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md">Placeholder<br></br><br></br><br></br>
+              <div className="py-5"> <p className=" text-center bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md">InteeliFridge<br></br><br></br><br></br>
               
               
               </p></div>
+
+
+              <a href="https://github.com/Capstone-project-Paris-Sal-Sergio-Joe/IntelliFridge" target="_blank">
+
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web5}
-              />
+                src={food}
+              /></a>
             </div>
 
             <div className="basis-1/3 flex-1">
