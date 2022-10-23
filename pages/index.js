d11f150be465c2c6ca1976b13d22a25a15b4af91
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-10-22 21:50:29 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import sal from "../public/sal.jpeg";
 import calc from "../public/calculator.png";
 import lara from "../public/lara.png";
 import poke from "../public/poke.png";
+import movie from "../public/movie.png";
 
 
 
@@ -126,27 +127,35 @@ Salvador Salazar            </h2>
               
               
               </p></div>
+
+<a href="https://github.com/salvadorsalazar/poke-app" target="_blank">
+
+
+
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
                 src={poke}
-              />
+              /></a>
             </div>
 
             <div className="basis-1/3 flex-1">
-              <div className="py-5"> <p className=" text-center bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md">Placeholder<br></br><br></br><br></br>
+              <div className="py-5"> <p className=" text-center bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md">Movie databse using react<br></br><br></br><br></br>
               
               
               </p></div>
+<a href="https://movieapp-sage.vercel.app/" target="_blank">
+
+
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web4}
-              />
+                src={movie}
+              /></a>
             </div>
 
 
