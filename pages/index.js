64ac48af4e3a262199ef69064dbd5781a5f2bd36
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-11-01 23:31:55 -0500

    link updates

--- pages/index.js
+++ pages/index.js
@@ -195,7 +195,7 @@ Salvador Salazar            </h2>
               /></a>
             </div>
 
-            <div className="basis-1/3 flex-1">
+            {/* <div className="basis-1/3 flex-1">
 
               <div className="py-5"> <p className=" text-center bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md">Placeholder<br></br><br></br><br></br>
               
@@ -208,7 +208,7 @@ Salvador Salazar            </h2>
                 layout="responsive"
                 src={web6}
               />
-            </div>
+            </div> */}
           </div>
         </section>
       </main>
