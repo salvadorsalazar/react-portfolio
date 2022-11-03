14a60a135e3b0ec4e80f880076b3eb18bae51951
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-11-02 20:48:57 -0500

    link updates

--- pages/index.js
+++ pages/index.js
@@ -82,7 +82,7 @@ Salvador Salazar            </h2>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
              Hello, I'm Salvador! 👋 I am a Junior Web Developer in San Antonio , Texas.
 
-📕 Skills: Javascript, HTML, CSS, Bootstrap, JQuery, Java, MySQL, Spring Boot, React.js , Next.js, C++ , C# , PHP Laravel . Exposure to: AWS ,Python
+📕 Skills: Javascript, HTML, CSS, Bootstrap, JQuery, Java, MySQL, Spring Boot, React.js , Next.js, C++ , C# , PHP Laravel . Exposure to: AWS ,Python .
             </p>
             {/* <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <AiFillTwitterCircle />
