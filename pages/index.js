11ea535a6052f3a23aa1e570fc8fd1baf2ddb561
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-12-03 23:18:08 -0600

    link updates, corrections

--- pages/index.js
+++ pages/index.js
@@ -178,7 +178,7 @@ Salvador Salazar            </h2>
 
             <div className="basis-1/3 flex-1">
 
-              <div className="py-5"> <p className=" text-center bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md">IntellilFridge<br></br><br></br><br></br>
+              <div className="py-5"> <p className=" text-center bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md">IntelliFridge<br></br><br></br><br></br>
               
               
               </p></div>
