f84dae47c37e5c0a5da9f1bdb4491ab7d3d922ec
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-12-15 18:31:25 -0600

    fix

--- pages/index.js
+++ pages/index.js
@@ -86,11 +86,7 @@ Salvador Salazar            </h2>
 
 📕 Skills: Javascript, HTML, CSS, Bootstrap, JQuery, Java, MySQL, Spring Boot, React.js , Next.js, C++ , C# , PHP Laravel . 
             </p>
-            {/* <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <AiFillTwitterCircle />
-              <a href="https://www.linkedin.com/in/salvador-salazar-73a45a21b/"><AiFillLinkedin /></a>
-              <AiFillYoutube />
-            </div> */}
+           
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative object-position: 50% 50% overflow-hidden mt-20 md:h-96 md:w-96">
               <Image src={sal} className="max-auto" layout="fill" objectFit="cover" />
             </div>
@@ -105,7 +101,6 @@ Salvador Salazar            </h2>
           <div className="flex flex-col gap-20 py-10 lg:flex-row lg:flex-wrap">
 
             <div className="basis-1/3 flex-1 ">
-              {/* <div>  <p className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8">Fully functional Calculator bult using React.js</p></div> */}
               <div className="py-5"> <p className=" text-center bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md">Basic Calculator built with React.js .<br></br><br></br><br></br>
               
               
@@ -149,7 +144,7 @@ Salvador Salazar            </h2>
 
 <a href="https://poke-app-2buu.vercel.app/" target="_blank">
 {/*  */}
-{/*  poke needs updating*/}
+{/*  poke needs updating...search function disabled*/}
 
               <Image
                 className="rounded-lg object-cover"
